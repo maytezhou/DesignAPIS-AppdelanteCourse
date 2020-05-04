476d884f2bb6d759348d27c3a7c007dbf3908077
maytezhou <maytezhou14@gmail.com>
2020-05-04 15:13:36 -0500

    'corrigiendo'

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const express = require('express');
 const bodyParser= require('body-parser');
 // id universal unico
-const uuidv4= require('uuid/v4');
+const uuidv4= require('uuid');
 
 
 const app = express();
 // servidor escuche en el puerto 3000
 
 const productos = [
-    {id :'1234' , titulo: 'Macbook Pro13 Inches', 
-    precio :1300 , moneda:'USD'},
-    {id :'5678' , titulo: 'Taza de Café', 
-    precio :10 , moneda:'USD'},
-    {id :'9101' , titulo: 'Micrófono blue yeti', 
-    precio :100, moneda:'USD'}
+    {id: '1234', titulo: 'Macbook Pro13 Inches', 
+    precio: 1300 , moneda: 'USD'},
+    {id: '5678', titulo: 'Taza de Café', 
+    precio: 10 , moneda: 'USD'},
+    {id: '9101', titulo: 'Micrófono blue yeti', 
+    precio: 100, moneda:'USD'}
     
 ]
 app.route('/productos') // ruta que nos permite acceder a la coleccion entera de los productos
@@ -27,7 +27,7 @@ let nuevoProducto =req.body
 //input validation
 if(!nuevoProducto.moneda || !nuevoProducto.precio || !nuevoProducto.titulo){
  //bad request tu request no es apropiado le falta algo - no cumple con los requisitos para ser un request válido
-    req.send('400').send("Tu producto deber especificar un titulo, precio y moneda")
+    req.send('400').send("Tu producto debe especificar un titulo, precio y moneda")
     return
 }
 nuevoProducto.id = uuidv4();
@@ -41,17 +41,17 @@ res.status(201).json(nuevoProducto)
 
 // ruta que nos permite acceder a cada producto
 // en la ruta se colocara el id del prodcuto como parametro
-app.get('/productos/:id',(req,res)=>{
+app.get('/productos/:id', (req, res)=>{
   //  req.params.id
   for(let producto of productos){
       if(producto.id == req.params.id){
           res.json(producto)
           return 
-      }
-      // Not found
-      res.status(404).send(`El Producto con id [
-          ${req.params.id}] no existe`)
+      }    
   }
+    // Not found
+    res.status(404).send(`El Producto con id [
+        ${req.params.id}] no existe`)
 
 });
 
