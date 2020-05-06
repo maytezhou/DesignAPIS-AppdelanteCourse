d4c6542f15026e3c85340a691cc6a2ab88a7d423
maytezhou <maytezhou14@gmail.com>
2020-05-06 10:06:53 -0500

    it is working now

--- api/recursos/productos/productos.routes.js
+++ api/recursos/productos/productos.routes.js
@@ -2,7 +2,6 @@ const express = require('express');
 const bodyParser= require('body-parser');
 const _ = require('underscore');
 const { v4: uuidv4 } = require('uuid');
-uuidv4(); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'
 //const Joi = require('@hapi/joi');
 const Joi = require('joi');
 
