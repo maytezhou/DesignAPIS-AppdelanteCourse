bfc3acc1225ae369d162a626c6251df181608447
maytezhou <maytezhou14@gmail.com>
2020-06-08 16:41:57 -0500

    conectando nuestros servidor a MongoDb

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const bodyParser= require('body-parser');
+const morgan = require('morgan')
+const mongoose = require('mongoose')
+
 const productosRouter = require('./api/recursos/productos/productos.routes')
 const usuariosRouter = require('./api/recursos/usuarios/usuarios.routes')
-const morgan = require('morgan')
 const logger = require('./utils/logger')
 const authJWT = require('./api/libs/auth')
 const config = require('./config')
@@ -12,7 +14,11 @@ const passport = require('passport')
 // autenticación básica
 
 passport.use(authJWT)
-
+mongoose.connect('mongodb://127.0.0.1:27017/vendetuscorotos') //API ADRESS localhost:puerto en el que corre mongo/databasename
+mongoose.connection.on('error',()=>{
+    logger.error('Falló la conexión a mongodb')
+    process.exit(1) // nuestro servicio no puede funcionar si nuestra base de datos no esta conectada
+})
 
 const app = express();
 // servidor escuche en el puerto 3000
