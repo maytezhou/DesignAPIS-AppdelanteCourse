d57271252cd09a06b506935965645bea910bba25
maytezhou <maytezhou14@gmail.com>
2020-06-08 12:11:32 -0500

    centralizar la configuración del servicio

---
+++ config/index.js
@@ -0,0 +1,24 @@
+const ambiente = process.env.NODE_ENV ||'development'
+const configuraciónBase ={
+    jwt:{ },
+    puerto:3000
+}
+let configuraciónDeAmbiente={}
+ switch (ambiente){
+case 'desarrollo':
+case 'dev':
+case 'development':
+  configuraciónDeAmbiente = require('./dev')
+break
+case 'producción':
+case 'prod':
+    configuraciónDeAmbiente = require('./prod')
+    break
+ default:
+    configuraciónDeAmbiente = require('./dev')
+
+ }
+ module.exports = {
+...configuraciónBase,
+...configuraciónDeAmbiente
+ }
