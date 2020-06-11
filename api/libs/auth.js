68258e326536d4f4afd047e0949f5f5967d210dc
maytezhou <maytezhou14@gmail.com>
2020-06-11 13:38:25 -0500

    migrar codigo de validación de tokens

--- api/libs/auth.js
+++ api/libs/auth.js
@@ -3,28 +3,36 @@ const bcrypt = require('bcrypt-nodejs')
 const passportJWT = require('passport-jwt')
 
 const log = require('./../../utils/logger')
-const usuarios = require('./../../database').usuarios
 const config = require('../../config')
+const usuarioController = require('../recursos/usuarios/usuarios.controller')
 
 
 // Token debe ser especificado mediante el header Authorization. Ejemplo:
 //Authorization : bearer xxxx.yyyy.zzzz
 
-let jwtOptions={
-    secretOrKey : config.jwt.secreto,
+let jwtOptions = {
+    secretOrKey: config.jwt.secreto,
     jwtFromRequest: passportJWT.ExtractJwt.fromAuthHeaderAsBearerToken()
 }
 
 module.exports = new passportJWT.Strategy(jwtOptions, (jwtPayload, next) => {
-    let index = _.findIndex(usuarios, usuario => usuario.id === jwtPayload.id)
-    
-    if (index === -1) {
-        log.info(`JWT token no es válido. Usuario con id  ${jwtPayload.id} no  existe`)
-        next(null, false)
-        
-    }else{
-        log.info(`Usuario ${usuarios[index].username} suministró un token válido. Auntenticación completada`)
- next(null,{ username:usuarios[index].username, id:usuarios[index].id })
-    }
+    usuarioController.obtenerUsuario({
+            id: jwtPayload.id
+        })
+        .then(usuario => {
+            if (!usuario) {
+                log.info(`JWT token no es válido. Usuario con id  ${jwtPayload.id} no  existe`)
+                next(null, false) // null significa que no hubo ningun error y false significa que fallo la validación del token 
+                return
+            }
+            log.info(`Usuario ${usuario.username} suministró un token válido. Auntenticación completada`)
+            next(null, { // el objeto que queremos agregar al request  con la propiedad user// request.user
+                username: usuario.username,
+                id: usuario.id
+            })
+        })
+        .catch((err) => {
+            log.error(`Error ocurrió al  tratar de validar un token`, err)
+            next(err) // o tambien puedes colocar next(err,false)
+        })
 })
-
